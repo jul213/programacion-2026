d7a3a92722357164c7cc2161bcf92ff21c367fc7
julio <158296107+jul213@users.noreply.github.com>
2026-03-22 10:25:00 +0000

    termminacion de programa simple de cifrado

--- cifrado.py
+++ cifrado.py
@@ -7,3 +7,14 @@ def cifrar_mensaje(mensaje, llave):
 
         resultado += chr(valor_cifrado) 
     return resultado
+
+LLAVE_SECRETA = 157 
+mensaje_original = "SISTEMA OPERATIVO COMPROMETIDO"
+
+
+encriptado = cifrar_mensaje(mensaje_original, LLAVE_SECRETA)
+print(f"Mensaje Cifrado (Basura para espías): {encriptado}")
+
+# Desciframos (Usando la misma lógica)
+desencriptado = cifrar_mensaje(encriptado, LLAVE_SECRETA)
+print(f"Mensaje Recuperado: {desencriptado}")
