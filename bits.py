0c527e28c23fb14358f7be4e47fa1bb30a77a4f2
julio <158296107+jul213@users.noreply.github.com>
2026-03-22 10:00:34 +0000

    desplazamos el bit

--- bits.py
+++ bits.py
@@ -1,3 +1,8 @@
 
 def revisar_luces(byte_entrada):
     print(f"Estado del sistema (Decimal: {byte_entrada}, Binario: {bin(byte_entrada)})")
+
+    for i in range(8):
+        mask = 1 << i
+        
+
